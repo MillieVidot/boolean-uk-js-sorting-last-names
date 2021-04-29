1da24100fd17f0f5a7288466eb2506e75fca8fd2
Camilla Vidot <camillavidot@gmail.com>
2021-04-29 09:44:47 +0100

    Fix 1 loop

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const lastName = ["Mason", "Marcora", "Rico", "Neves", "Ivanov"]
+const lastNameArr = ["Mason", "Marcora", "Rico", "Neves", "Ivanov"]
 let userLastName = prompt(`What is your last name?`)
-lastName.push(userLastName)
-console.log(lastName.sort())
-console.log(`My position in the array is:${lastName.indexOf("Vidot")}`)
-console.log((lastName.toString()).toUpperCase())
+lastNameArr.push(userLastName)
+console.log(lastNameArr.sort())
+console.log(`My position in the array is:${lastNameArr.indexOf("Vidot")}`)
+
+newArray = []
+for (const lastName of lastNameArr) {
+   newArray.push([lastName , lastName.toUpperCase()])
+}
+console.log(newArray)
+
+
+// for (const lastName of lastNameArr) {
+//    console.log(lastName.toUpperCase())
+// }
+
+// for (let i = 0; i < lastNameArr.length; i++) {
+//    newArray.push([lastNameArr[i] , uppercaseNames[i]])
+//    console.log(newArray)
+// }
+
+
+
+
+
+
